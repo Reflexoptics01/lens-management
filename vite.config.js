17eed8feb4b5005f5bf2c985e3ebfdf37858abfe
Reflexoptics01 <reflexopticsolutions@gmail.com>
2025-02-19 09:06:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',
-  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 5174,
-    host: true
+    outDir: 'dist'
   }
 }) 
